174ced17305abbb8d7a8c957b15136d017351703
Dirk Kok <dirk@vorteq.nl>
2024-10-10 12:26:07 +0200

    un-hardcode "2 days away"

--- CanYouBelieveItGuysWebfishing/Program.cs
+++ CanYouBelieveItGuysWebfishing/Program.cs
@@ -3,7 +3,7 @@ using DSharpPlus;
 
 var format = @"can you believe it guys? webfishing! just {0} hours away!
 webfishing is in {0} hours! woohoo! i am so happy about this information!
-webfishing just 2 days away, oh wow! can you believe it? webfishing!
+webfishing just {1} days away, oh wow! can you believe it? webfishing!
 just in {0} hours! it got here so fast! webfishing! just {0} h";
 
 var regex = new Regex("web ?fishing", RegexOptions.IgnoreCase);
@@ -19,7 +19,7 @@ discord.MessageCreated += async (_, eventArgs) => {
         var webfishing = new DateTime(2024, 10, 12, 1, 0, 0, DateTimeKind.Utc);
         var timeToWebfishing = webfishing - DateTime.UtcNow;
 
-        await eventArgs.Message.RespondAsync(string.Format(format, (int) timeToWebfishing.TotalHours));
+        await eventArgs.Message.RespondAsync(string.Format(format, (int) timeToWebfishing.TotalHours, Math.Ceiling(timeToWebfishing.TotalDays)));
     }
 };
 
