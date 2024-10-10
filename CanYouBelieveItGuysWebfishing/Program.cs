cb147ceb477c2587704e5ec4c7f7bb93ba4bbf47
Dirk Kok <the@dirkkok.nl>
2024-10-10 16:16:06 +0200

    response on one line; move release date variable to outer scope

--- CanYouBelieveItGuysWebfishing/Program.cs
+++ CanYouBelieveItGuysWebfishing/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System.Text.RegularExpressions;
 using DSharpPlus;
 
-var format = @"can you believe it guys? webfishing! just {0} hours away!
-webfishing is in {0} hours! woohoo! i am so happy about this information!
-webfishing just {1} days away, oh wow! can you believe it? webfishing!
-just in {0} hours! it got here so fast! webfishing! just {0} h";
-
+var format = @"can you believe it guys? webfishing! just {0} hours away! webfishing is in {0} hours! woohoo! i am so happy about this information! webfishing just {1} days away, oh wow! can you believe it? webfishing! just in {0} hours! it got here so fast! webfishing! just {0} h";
+var webfishing = new DateTime(2024, 10, 11, 19, 0, 0, DateTimeKind.Utc);
 var regex = new Regex("web ?fishing", RegexOptions.IgnoreCase);
 
 var discord = new DiscordClient(new DiscordConfiguration() {
@@ -16,7 +13,6 @@ var discord = new DiscordClient(new DiscordConfiguration() {
 
 discord.MessageCreated += async (_, eventArgs) => {
     if (!eventArgs.Author.IsCurrent && regex.IsMatch(eventArgs.Message.Content)) {
-        var webfishing = new DateTime(2024, 10, 11, 19, 0, 0, DateTimeKind.Utc);
         var timeToWebfishing = webfishing - DateTime.UtcNow;
 
         await eventArgs.Message.RespondAsync(string.Format(format, (int) timeToWebfishing.TotalHours, Math.Ceiling(timeToWebfishing.TotalDays)));
