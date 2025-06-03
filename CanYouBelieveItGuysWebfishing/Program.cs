e27ae2fd2a30a93bccad1aef5520d6add2d98e07
Dirk Kok <the@dirkkok.nl>
2025-06-03 19:33:50 +0200

    envvars for everything

--- CanYouBelieveItGuysWebfishing/Program.cs
+++ CanYouBelieveItGuysWebfishing/Program.cs
@@ -1,21 +1,28 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using DSharpPlus;
 
-var format = @"can you believe it guys? webfishing! just {0} hours away! webfishing is in {0} hours! woohoo! i am so happy about this information! webfishing just {1} days away, oh wow! can you believe it? webfishing! just in {0} hours! it got here so fast! webfishing! just {0} h";
-var webfishing = new DateTime(2024, 10, 11, 19, 0, 0, DateTimeKind.Utc);
-var regex = new Regex("web ?fishing", RegexOptions.IgnoreCase);
+string GetEnv(string key) {
+    return Environment.GetEnvironmentVariable(key) ?? throw new Exception($"Missing environment variable {key}");
+}
+
+var format = @"can you believe it guys? {2}! just {0} hours away! {2} is in {0} hours! woohoo! i am so happy about this information! {2} just {1} days away, oh wow! can you believe it? {2}! just in {0} hours! it got here so fast! {2}! just {0} h";
+var date = DateTime.ParseExact(GetEnv("DATE"), "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+var regex = new Regex(GetEnv("REGEX"), RegexOptions.IgnoreCase);
+var webfishing = GetEnv("WEBFISHING");
 
 var discord = new DiscordClient(new DiscordConfiguration() {
-    Token = Environment.GetEnvironmentVariable("DISCORD_TOKEN"),
+    Token = GetEnv("DISCORD_TOKEN"),
     TokenType = TokenType.Bot,
     Intents = DiscordIntents.GuildMessages | DiscordIntents.DirectMessages | DiscordIntents.MessageContents,
 });
 
 discord.MessageCreated += async (_, eventArgs) => {
     if (!eventArgs.Author.IsCurrent && regex.IsMatch(eventArgs.Message.Content)) {
-        var timeToWebfishing = webfishing - DateTime.UtcNow;
+        var timeToWebfishing = date - DateTime.UtcNow;
 
-        await eventArgs.Message.RespondAsync(string.Format(format, (int) timeToWebfishing.TotalHours, Math.Ceiling(timeToWebfishing.TotalDays)));
+        await eventArgs.Message.RespondAsync(string.Format(format, (int) timeToWebfishing.TotalHours, Math.Ceiling(timeToWebfishing.TotalDays), webfishing));
     }
 };
 
