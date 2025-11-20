b18505d32db62ef198ebb4fa5b333cccee048647
Dirk Kok <dirk@vorteq.nl>
2025-11-20 12:36:58 +0100

    make more robust (idk wtf i was thinking with the previous setup)

--- CanYouBelieveItGuysWebfishing/Program.cs
+++ CanYouBelieveItGuysWebfishing/Program.cs
@@ -4,26 +4,63 @@ using System.Text.RegularExpressions;
 using DSharpPlus;
 
 string GetEnv(string key) {
-    return Environment.GetEnvironmentVariable(key) ?? throw new Exception($"Missing environment variable {key}");
+	return Environment.GetEnvironmentVariable(key) ?? throw new Exception($"Missing environment variable {key}");
 }
 
-var format = @"can you believe it guys? {2}! just {0} hours away! {2} is in {0} hours! woohoo! i am so happy about this information! {2} just {1} days away, oh wow! can you believe it? {2}! just in {0} hours! it got here so fast! {2}! just {0} h";
+var format = @"can you believe it guys? {0}! just {1} {2} away! {0} is in {1} {2}! woohoo! i am so happy about this information! {0} just {1} {2} away, oh wow! can you believe it? {0}! just in {1} {2}! it got here so fast! {0}! just {1} {3}";
 var date = DateTime.ParseExact(GetEnv("DATE"), "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
 var regex = new Regex(GetEnv("REGEX"), RegexOptions.IgnoreCase);
 var webfishing = GetEnv("WEBFISHING");
 
 var discord = new DiscordClient(new DiscordConfiguration() {
-    Token = GetEnv("DISCORD_TOKEN"),
-    TokenType = TokenType.Bot,
-    Intents = DiscordIntents.GuildMessages | DiscordIntents.DirectMessages | DiscordIntents.MessageContents,
+	Token = GetEnv("DISCORD_TOKEN"),
+	TokenType = TokenType.Bot,
+	Intents = DiscordIntents.GuildMessages | DiscordIntents.DirectMessages | DiscordIntents.MessageContents,
 });
 
 discord.MessageCreated += async (_, eventArgs) => {
-    if (!eventArgs.Author.IsCurrent && regex.IsMatch(eventArgs.Message.Content)) {
-        var timeToWebfishing = date - DateTime.UtcNow;
+	if (!eventArgs.Author.IsCurrent && regex.IsMatch(eventArgs.Message.Content)) {
+		TimeSpan timeToWebfishing = date - DateTime.UtcNow;
 
-        await eventArgs.Message.RespondAsync(string.Format(format, (int) timeToWebfishing.TotalHours, Environment.GetEnvironmentVariable("JUSTAWEEKAWAY") ?? $"just {Math.Ceiling(timeToWebfishing.TotalDays)} hours away", webfishing));
-    }
+		int timeQuantity;
+		string? timeQuantityString = null;
+		string timeUnit;
+		if (timeToWebfishing.TotalHours < 24) {
+			timeQuantity = (int) timeToWebfishing.TotalHours;
+			timeUnit = "hour";
+
+			if (timeQuantity == 1) {
+				timeQuantityString = "an";
+			}
+		} else if (timeToWebfishing.TotalDays < 6) { // on purpose, say "a week" if it's 6 days
+			timeQuantity = (int) timeToWebfishing.TotalDays;
+			timeUnit = "day";
+
+			if (timeQuantity == 1) {
+				timeQuantityString = "a";
+			}
+		} else {
+			timeQuantity = (int) (timeToWebfishing.TotalDays / 7);
+			if (timeQuantity == 0) {
+				timeQuantity = 1;
+			}
+			timeUnit = "week";
+
+			if (timeQuantity == 1) {
+				timeQuantityString = "a";
+			}
+		}
+
+		if (timeQuantity > 1) {
+			timeUnit += "s";
+		}
+		
+		string timeUnitCutOff = timeUnit[0].ToString();
+
+		timeQuantityString ??= timeQuantity.ToString();
+		
+		await eventArgs.Message.RespondAsync(string.Format(format, webfishing, timeQuantityString, timeUnit, timeUnitCutOff));
+	}
 };
 
 await discord.ConnectAsync();
